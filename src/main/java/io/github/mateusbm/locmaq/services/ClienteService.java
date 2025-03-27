372aee081808d5ef1b46ee5c10c7ce1299a57732
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-27 17:40:11 -0300

    feat: Criando classe service do Cliente

---
+++ src/main/java/io/github/mateusbm/locmaq/services/ClienteService.java
@@ -0,0 +1,12 @@
+package io.github.mateusbm.locmaq.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    public void cadastrarCliente() {}
+    public void editarCliente(){}
+    public void consultarCliente(){}
+}
