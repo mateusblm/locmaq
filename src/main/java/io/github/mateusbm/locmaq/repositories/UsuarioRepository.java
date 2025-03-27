0d54c7aed00c69dbe20a125a0d88e1cfab3be94f
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-27 17:41:23 -0300

    feat: Criando classe repository do Usuário

---
+++ src/main/java/io/github/mateusbm/locmaq/repositories/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package io.github.mateusbm.locmaq.repositories;
+
+import io.github.mateusbm.locmaq.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
