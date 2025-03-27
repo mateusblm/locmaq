11889a7fbe307ae5715677e0ba4d394605020e4b
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-27 17:41:33 -0300

    feat: Criando classe service do Usuário

---
+++ src/main/java/io/github/mateusbm/locmaq/services/UsuarioService.java
@@ -0,0 +1,13 @@
+package io.github.mateusbm.locmaq.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    public void realizarLogin() {
+    }
+
+    public void registrarDados(){}
+}
