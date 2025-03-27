00699b3a175e373a6153852fa72cdfb302fb2341
mateusblm <mateusblm@outlook.com>
2025-03-27 11:58:51 -0300

    Iniciando o projeto

---
+++ src/main/java/io/github/mateusbm/locmaq/LocMaqApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateusbm.locmaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocMaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocMaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateusbm/locmaq/LocMaqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusbm.locmaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocMaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
