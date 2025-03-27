cc6d469966d9559de088f4ef50b22f79f901b02a
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-27 17:39:37 -0300

    feat: Criando classe service do BoletimMedicao

---
+++ src/main/java/io/github/mateusbm/locmaq/services/BoletimMedicaoService.java
@@ -0,0 +1,12 @@
+package io.github.mateusbm.locmaq.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoletimMedicaoService {
+
+    public void cadastrarBoletim() {}
+    public void editarBoletim() {}
+    public void consultarBoletim() {}
+}
