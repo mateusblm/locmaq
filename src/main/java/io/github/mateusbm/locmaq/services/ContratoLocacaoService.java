8bdd4da6668319dfd08e098db09e6b78d7b184eb
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-27 17:40:36 -0300

    feat: Criando classe service do Contrato de Locação

---
+++ src/main/java/io/github/mateusbm/locmaq/services/ContratoLocacaoService.java
@@ -0,0 +1,13 @@
+package io.github.mateusbm.locmaq.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContratoLocacaoService {
+
+    public void criarContrato() {}
+    public void editarContrato() {}
+    public void cancelarContrato() {}
+    public void calcularValorTotal() {}
+}
