24486afa3e455315d9c65023a1231b2b9a60a36d
Mateus Burlamaqui <mateusblm@outlook.com>
2025-03-27 17:41:04 -0300

    feat: Criando classe service do Equipamento

---
+++ src/main/java/io/github/mateusbm/locmaq/services/EquipamentoService.java
@@ -0,0 +1,15 @@
+package io.github.mateusbm.locmaq.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EquipamentoService {
+
+    public void cadastrarEquipamento(){
+    }
+
+    public void editarEquipamento() {}
+
+    public void verificarDisponibilidade() {}
+}
