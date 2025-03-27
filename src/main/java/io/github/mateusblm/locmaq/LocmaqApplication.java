630cb1373e94cb522a0c789e83fbcc5e4918a898
Mateus Burlamaqui Moreira <mateus.burlamaqui@pucpr.edu.br>
2025-03-27 10:21:19 -0300

    Initial commit

---
+++ src/main/java/io/github/mateusblm/locmaq/LocmaqApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateusblm.locmaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocmaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocmaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateusblm/locmaq/LocmaqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusblm.locmaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocmaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
