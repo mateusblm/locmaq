fd0a328037a6acadbc64af753b2ed438c74bc811
Mateus Burlamaqui <mateusblm@outlook.com>
2025-04-14 17:17:34 -0300

    feat: Criando DataInitializer, classe que cria um usuário admin ao iniciar a aplicação.

---
+++ src/main/java/io/github/mateusbm/locmaq/config/DataInitializer.java
@@ -0,0 +1,24 @@
+package io.github.mateusbm.locmaq.config;
+import io.github.mateusbm.locmaq.models.TipoUsuario;
+import io.github.mateusbm.locmaq.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (!usuarioService.existeUsuarioPorNome("admin")) {
+            usuarioService.cadastrarUsuario("admin", "root", TipoUsuario.GESTOR);
+            System.out.println("Usuário admin criado com sucesso.");
+        }
+        else {
+            System.out.println("Usuario admin já criado");
+        }
+    }
+}
