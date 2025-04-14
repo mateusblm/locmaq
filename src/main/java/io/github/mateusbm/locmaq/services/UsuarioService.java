8ccd7ca85cf2a1c05716197350c8bcd65a6d86a3
Mateus Burlamaqui <mateusblm@outlook.com>
2025-04-14 17:20:39 -0300

    feat: Adicionando funcionalidades de cadastrarUsuario, autenticação, e existeUsuarioPorNome

--- src/main/java/io/github/mateusbm/locmaq/services/UsuarioService.java
+++ src/main/java/io/github/mateusbm/locmaq/services/UsuarioService.java
@@ -1,13 +1,33 @@
 package io.github.mateusbm.locmaq.services;
 
 
+import io.github.mateusbm.locmaq.models.TipoUsuario;
+import io.github.mateusbm.locmaq.models.Usuario;
+import io.github.mateusbm.locmaq.repositories.UsuarioRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-    public void realizarLogin() {
+    public Usuario autenticar(String nome, String senha) {
+        Usuario usuario = usuarioRepository.findByNome(nome);
+        if (usuario != null && usuario.getSenha().equals(senha)) {
+            return usuario;
+        }
+        return null;
     }
-
-    public void registrarDados(){}
+    @Transactional
+    public void cadastrarUsuario(String nome, String senha, TipoUsuario tipoUsuario) {
+        Usuario novoUsuario = new Usuario(nome, senha, tipoUsuario);
+        usuarioRepository.save(novoUsuario);
+    }
+    public boolean existeUsuarioPorNome(String nome) {
+        return usuarioRepository.findByNome(nome) != null;
+    }
 }
+
