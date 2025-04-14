6c36b468dab290a0bcc73346a12311b91db1f4c0
Mateus Burlamaqui <mateusblm@outlook.com>
2025-04-14 17:20:01 -0300

    feat: Adicionando funcionalidade de findByName por necessidade de outras classes.

--- src/main/java/io/github/mateusbm/locmaq/repositories/UsuarioRepository.java
+++ src/main/java/io/github/mateusbm/locmaq/repositories/UsuarioRepository.java
@@ -4,4 +4,6 @@ import io.github.mateusbm.locmaq.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByNome(String nome);
+
 }
