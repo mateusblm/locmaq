d33e8376aaee94dd1ace15acb5449b8a0f171ea2
Erik-Carvalho <erikcarvalho2016@outlook.com.br>
2025-04-17 13:59:28 -0300

    feat: Criando classe controller do contratoLocacao e ligando ao service.

---
+++ src/main/java/io/github/mateusbm/locmaq/controllers/ContratoLocacaoController.java
@@ -0,0 +1,9 @@
+package io.github.mateusbm.locmaq.controllers;
+
+import io.github.mateusbm.locmaq.services.ContratoLocacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ContratoLocacaoController {
+    @Autowired
+    private ContratoLocacaoService contratolocacaoservice;
+}
