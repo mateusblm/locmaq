a4b4654db9e6c8a88e1d32dc50508bafff0797ab
Erik-Carvalho <erikcarvalho2016@outlook.com.br>
2025-04-17 13:55:33 -0300

    feat: Criando conexão entre repository e service

--- src/main/java/io/github/mateusbm/locmaq/services/ContratoLocacaoService.java
+++ src/main/java/io/github/mateusbm/locmaq/services/ContratoLocacaoService.java
@@ -1,10 +1,17 @@
 package io.github.mateusbm.locmaq.services;
 
 
+import io.github.mateusbm.locmaq.repositories.ContratoLocacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContratoLocacaoService {
+
+    @Autowired
+    private ContratoLocacaoRepository contratoLocacaoRepository
+
+
 
     public void criarContrato() {}
     public void editarContrato() {}
