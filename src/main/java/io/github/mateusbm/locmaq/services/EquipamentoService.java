2c3b6f52a310b64fc0c1846010128810bc385769
Erick Coradin <erickcoradin@gmail.com>
2025-04-16 22:35:32 -0300

    feat: criando conexão entre Repository e Service

--- src/main/java/io/github/mateusbm/locmaq/services/EquipamentoService.java
+++ src/main/java/io/github/mateusbm/locmaq/services/EquipamentoService.java
@@ -1,10 +1,15 @@
 package io.github.mateusbm.locmaq.services;
 
 
+import io.github.mateusbm.locmaq.repositories.EquipamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EquipamentoService {
+
+    @Autowired
+    private EquipamentoRepository
 
     public void cadastrarEquipamento(){
     }
