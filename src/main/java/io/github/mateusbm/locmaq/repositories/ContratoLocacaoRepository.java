caef6a763bcd24c21d4a7183db91f734ccbc3164
Erik-Carvalho <erikcarvalho2016@outlook.com.br>
2025-04-17 13:52:17 -0300

    feat: Criando repository ContratoLocacao

---
+++ src/main/java/io/github/mateusbm/locmaq/repositories/ContratoLocacaoRepository.java
@@ -0,0 +1,7 @@
+package io.github.mateusbm.locmaq.repositories;
+
+import io.github.mateusbm.locmaq.models.ContratoLocacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContratoLocacaoRepository extends JpaRepository<ContratoLocacao, Long> {
+}
