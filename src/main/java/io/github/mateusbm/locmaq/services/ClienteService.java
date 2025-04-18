b90c6ec6f07977bcb23a94f7e6fbc554f738e323
Luca Medina <114019168+lumas888@users.noreply.github.com>
2025-04-18 17:22:56 -0300

    Conexão entre repository e service cliente

--- src/main/java/io/github/mateusbm/locmaq/services/ClienteService.java
+++ src/main/java/io/github/mateusbm/locmaq/services/ClienteService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClienteService {
 
+    @Autowired
+    private ClienteRepository clienterepository;
+
     public void cadastrarCliente() {}
     public void editarCliente(){}
     public void consultarCliente(){}
