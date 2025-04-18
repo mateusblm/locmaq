a998bd47831c7380270c2e9f9ef0d134826ec0ed
Luca Medina <114019168+lumas888@users.noreply.github.com>
2025-04-18 17:30:26 -0300

    criação da Classe controller do cliente e ligação ao service

---
+++ src/main/java/io/github/mateusbm/locmaq/controllers/ClienteController.java
@@ -0,0 +1,9 @@
+package io.github.mateusbm.locmaq.controllers;
+
+import io.github.mateusbm.locmaq.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ClienteController {
+    @Autowired
+    private ClienteService clienteservice;
+}
