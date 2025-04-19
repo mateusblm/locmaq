e8f943e85aab9ad3ef606d8a141e8ea1a8f9aa8b
Luca Medina <114019168+lumas888@users.noreply.github.com>
2025-04-19 20:47:32 -0300

    preenchendo classe cliente controller

--- src/main/java/io/github/mateusbm/locmaq/controllers/ClienteController.java
+++ src/main/java/io/github/mateusbm/locmaq/controllers/ClienteController.java
@@ -1,9 +1,40 @@
 package io.github.mateusbm.locmaq.controllers;
 
+import io.github.mateusbm.locmaq.models.Cliente;
 import io.github.mateusbm.locmaq.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
     @Autowired
-    private ClienteService clienteservice;
+    private ClienteService clienteService;
+
+    @GetMapping
+    public List<Cliente> listarTodosClientes() {
+        return clienteService.listarTodosClientes();
+    }
+
+    @PostMapping
+    public Cliente criar(@RequestBody Cliente cliente) {
+        return clienteService.cadastrarCliente(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        return clienteService.atualizarCliente(id, cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable Long id) {
+        clienteService.removerCliente(id);
+    }
+
+    @GetMapping("/{id}")
+    public Cliente buscarPorId(@PathVariable Long id) {
+        return clienteService.buscarClientePorId(id).orElse(null);
+    }
 }
