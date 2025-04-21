13239297f9afedbee68c6d1859e19d6439be7217
Erik-Carvalho <erikcarvalho2016@outlook.com.br>
2025-04-20 22:54:34 -0300

    feat:Completando classe BoletimMedicao

--- src/main/java/io/github/mateusbm/locmaq/models/BoletimMedicao.java
+++ src/main/java/io/github/mateusbm/locmaq/models/BoletimMedicao.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table
@@ -19,6 +21,26 @@ public class BoletimMedicao {
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String periodo;
+
+    @ManyToOne
+    private Usuario planejador;
+
+    private String situacao;
+
+    @Column(nullable = false)
+    private LocalDate dataMedicao;
+
+    @OneToMany(mappedBy = "boletimMedicao", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<EquipamentoBoletimMedicao> equipamentos = new ArrayList<>();
+
+    private Boolean assinado = false;
+    private String observacoes;
+
+    public BoletimMedicao() {
+
+    }
 
 
     @Column(nullable = false)
